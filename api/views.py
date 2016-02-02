e14d93d941cc2f25ed3cb62d7853857b4808a33e
fspinelli <kenyspinelli@hotmail.com>
2016-02-02 01:31:33 -0200

    .

--- api/views.py
+++ api/views.py
@@ -8,6 +8,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.contrib.auth.models import User, Permission, Group
+from django.contrib.auth import authenticate, login as auth_login, logout
 from api.forms import *
 from api.models import *
 
@@ -26,17 +27,38 @@ def signup(request):
                 register.save()
                 user = User.objects.filter(pk=register.pk)
                 user_serializer = serializers.serialize("json", user)
-                token = jwt.encode({request.GET.get('username',False): request.GET.get('password',False)}, 'secret', algorithm='HS256')
                 a = AccountSystem()
                 a.name = request.GET.get('name',False)
                 a.save()
                 user_profile = UserProfile()
                 user_profile.user = User.objects.get(pk=register.pk)
-                user_profile.access_token = token
                 user_profile.account_system = a
                 user_profile.save()
-                return HttpResponse('[{"token":"'+token+'", "data":"'+user_serializer+'"}]')
+                return HttpResponse(user_serializer)
             except:
                 return HttpResponse('500')
         else:
             return HttpResponse(register_form.errors)
+
+@csrf_exempt
+def signin(request):
+    if request.method == 'GET':
+        username = request.GET['username']
+        password = request.GET['password']
+        user = authenticate(username=username, password=password)
+        if user:
+            if user.is_active:
+                token = jwt.encode({request.GET.get('username',False): request.GET.get('password',False)}, 'secret', algorithm='HS256')
+
+                user = User.objects.filter(pk=user.pk)
+                user_profile = UserProfile.objects.filter(user=user)
+                user_profile.update(access_token=token)
+                
+                user_serializer = serializers.serialize("json", user)
+                userprofile_serializer = serializers.serialize("json", user_profile)
+                return HttpResponse('[{"user":"'+user_serializer+'", "user_profile":"'+userprofile_serializer+'"}]')
+            else:
+                return HttpResponse("{'mensagem':'Seu usuário está desativado.'}")
+
+        else:
+            return HttpResponse("{'mensagem':'Usuário ou senha incorreta.'}")
---
+++ app/app/controller.js
@@ -0,0 +1,4 @@
+app.controller('RegisterCtrl', function($rootScope, $location)
+{
+   
+});
